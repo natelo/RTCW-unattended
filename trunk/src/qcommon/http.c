a643d7cce5b90f197062c7b046490ed36396ebf9
nate <nate@4381cfe6-fa9a-f442-b6f5-31fa80d72593>
2014-11-09 14:12:06 +0000

    - Added libcurl - Sorted project so it statically links it - Added test in http.c to see if all is working..
    git-svn-id: svn://svn.rocksteady-hosting.com/rtcwMPClient@75 4381cfe6-fa9a-f442-b6f5-31fa80d72593

--- trunk/src/qcommon/http.c
+++ trunk/src/qcommon/http.c
@@ -22,9 +22,38 @@ Last Updated: 26.07 / 14
 
 #include "http.h"
 
+#include "curl/curl.h"
+#include "curl/easy.h"
+
+//#include <curl/curl.h>
+//#include <curl/easy.h>
+
+
 // Easier to read..
 #define _SEND(SOCK, MSG) \
 	send(SOCK, MSG, (int)strlen(MSG), 0);
+
+void test(void) {
+	CURL *curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+		/* example.com is redirected, so we tell libcurl to follow redirection */
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+		/* Perform the request, res will get the return code */
+		res = curl_easy_perform(curl);
+		/* Check for errors */
+		if (res != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+			curl_easy_strerror(res));
+
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+	}
+}
 
 /*
 ===============
