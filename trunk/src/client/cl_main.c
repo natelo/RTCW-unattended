9f21caa092013dff587beef4a0562d79677e6457
nate <nate@4381cfe6-fa9a-f442-b6f5-31fa80d72593>
2014-11-27 23:54:29 +0000

    Fixed MOTD so it shows up in map loading as well..
    git-svn-id: svn://svn.rocksteady-hosting.com/rtcwMPClient@183 4381cfe6-fa9a-f442-b6f5-31fa80d72593

--- trunk/src/client/cl_main.c
+++ trunk/src/client/cl_main.c
@@ -942,10 +942,43 @@ void CL_ForwardCommandToServer( const char *string ) {
 ===================
 CL_RequestMotd
 
+===================
+*/
+void CL_RequestMotd(void) {
+	char info[MAX_INFO_STRING];
+
+	if (!cl_motd->integer) {
+		return;
+	}
+	Com_Printf("Resolving %s\n", UPDATE_SERVER_NAME);
+	if (!NET_StringToAdr(UPDATE_SERVER_NAME, &cls.updateServer)) {
+		Com_Printf("Couldn't resolve address\n");
+		return;
+	}
+	cls.updateServer.port = BigShort(PORT_UPDATE);
+	Com_Printf("%s resolved to %i.%i.%i.%i:%i\n", UPDATE_SERVER_NAME,
+		cls.updateServer.ip[0], cls.updateServer.ip[1],
+		cls.updateServer.ip[2], cls.updateServer.ip[3],
+		BigShort(cls.updateServer.port));
+
+	info[0] = 0;
+	Com_sprintf(cls.updateChallenge, sizeof(cls.updateChallenge), "%i", rand());
+
+	Info_SetValueForKey(info, "challenge", cls.updateChallenge);
+	Info_SetValueForKey(info, "renderer", cls.glconfig.renderer_string);
+	Info_SetValueForKey(info, "version", com_version->string);
+
+	NET_OutOfBandPrint(NS_CLIENT, cls.updateServer, "getmotd \"%s\"\n", info);
+}
+
+/*
+===================
+CL_RequestMotd
+
 L0 - Patched for HTTP
 ===================
 */
-void CL_RequestMotd( void ) {
+void CL_infoRequestMotd( void ) {
 	char *result;
 
 	if ( !cl_motd->integer ) {
@@ -2738,7 +2771,7 @@ void CL_CheckAutoUpdate( void ) {
 	}
 
 	// Fetch MOTD..
-	CL_RequestMotd();
+	CL_infoRequestMotd();
 	
 	autoupdateChecked = qtrue;
 }
